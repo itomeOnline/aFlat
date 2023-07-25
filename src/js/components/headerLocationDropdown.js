56502d8e79eb431936fe778b56d1e757476ec882
Arina Gerdasova <arinoko99@gmail.com>
2023-07-25 20:19:40 +0700

    Верстка меню

--- src/js/components/headerLocationDropdown.js
+++ src/js/components/headerLocationDropdown.js
@@ -1,25 +1,28 @@
 function headerLocationDropdown() {
-    const block = document.querySelector('[data-location-dropdown-block]');
-    const btn = block.querySelector('[data-location-dropdown-main-btn]');
-    const btnClose = block.querySelector('[data-btn-close]');
-    const clickChangeBtns = block.querySelectorAll('[data-click-change]');
-    const clickChangeText = block.querySelector('[data-change-text=""]');
+    const blocks = document.querySelectorAll('[data-location-dropdown-block]');
 
-
-    btn.addEventListener('click', _ => {
-        block.classList.toggle('is-open');
-    })
-
-    btnClose.addEventListener('click', _ => {
-        block.classList.remove('is-open');
-    })
-
-    clickChangeBtns.forEach(btn => {
+    blocks.forEach(block => {
+        const btn = block.querySelector('[data-location-dropdown-main-btn]');
+        const btnClose = block.querySelector('[data-btn-close]');
+        const clickChangeBtns = block.querySelectorAll('[data-click-change]');
+        const clickChangeText = block.querySelector('[data-change-text=""]');
+   
         btn.addEventListener('click', _ => {
-            clickChangeText.textContent = btn.textContent;
+            block.classList.toggle('is-open');
+        })
+    
+        btnClose.addEventListener('click', _ => {
             block.classList.remove('is-open');
         })
+    
+        clickChangeBtns.forEach(btn => {
+            btn.addEventListener('click', _ => {
+                clickChangeText.textContent = btn.textContent;
+                block.classList.remove('is-open');
+            })
+        })
     })
+
 }
 
 export default headerLocationDropdown;
